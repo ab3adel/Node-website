97c1abf0f782e74ff5b4a005d1baa24820461d63
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2021-06-08 01:06:12 +0300

    Update app.tsx

--- src/comp/app.tsx
+++ src/comp/app.tsx
@@ -3,6 +3,12 @@ import {Route,Link,Switch} from 'react-router-dom'
 import {Myself} from './myself'
 import {Home} from './home'
 import {About} from './about'
+import {Footer} from './footer'
+import {I} from './iam'
+import github from '../images/github.png'
+import fb from '../images/facebook.png'
+import tele from '../images/telegram.png'
+
  class App extends React.Component <{},{}>{
        constructor (props:any) {
            super(props)
@@ -15,24 +21,36 @@ import {About} from './about'
             <div className= "main" >
                 <div className="header">
                 
-               
+               <Link to ="/">Home</Link>
                 <Link to ='/about'>About</Link>
-                <Link to ="/">Home</Link>
+                <Link to ="/portofolio">Portofolio</Link>
                 </div>
       
-                <Myself/>
+          
+             
                
-                <div id="footer">
-                    <span></span>
-                    <span> </span>
-                    <span> </span>
-                    <span> </span>
-                </div>
                 <Switch>
                   
-                    <Route path="/about"> <About/></Route>
-                    <Route path="/"> <Home/></Route>
+                    <Route exact path="/i"> <I/></Route>
+                    <Route exact path="/about"> <About/></Route>
+                    <Route exact path="/portofolio"> <Home/></Route>
+                    <Route path="/">   
+                       <Myself/>
+                       <Footer/>
+                    </Route>
+                    
                 </Switch>
+                <div className="last">
+                <a href="https://github.com/ab3adel">
+                    <img src={fb} className="fbSocial"/>
+                    </a>
+                    <a href="https://github.com/ab3adel">
+                    <img src={tele} className="teleSocial"/>
+                    </a>
+                    <a href="https://github.com/ab3adel">
+                    <img src={github} className="githubSocial"/>
+                        </a>
+                </div>
             </div>
         )
     }
