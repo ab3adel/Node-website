87b597ff084153d7fea23d8e9442d6220ddf5961
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2021-06-08 01:04:32 +0300

    Add files via upload

---
+++ src/comp/footer.tsx
@@ -0,0 +1,52 @@
+import React,{useEffect} from 'react'
+
+import  {Link} from 'react-router-dom'
+
+import * as d3 from 'd3'
+interface iselect extends Selection {_group:[HTMLElement],_parents:any}
+export const Footer =()=>{
+    useEffect(()=>{
+        animateFooter();
+    })
+ const animateFooter =()=>{
+     let link = document.getElementsByClassName('footerLink')[0] as HTMLDivElement;
+     let link1=document.getElementsByClassName('footerLink1')[0] as HTMLDivElement;
+      link.style.display="flex";
+      link.style.animationName="linkMove";
+      link1.style.display="flex";
+      link1.style.animationName="linkMove";
+ }
+ const lighten=(e:React.MouseEvent | React.TouchEvent) =>{
+   let target= e.target as HTMLDivElement
+   target.classList.add('lightenLink');
+ }
+ const dtLighten =(e:React.MouseEvent | React.TouchEvent)=>{
+    let target= e.target as HTMLDivElement
+    target.classList.remove('lightenLink');
+ }
+return (
+    <div id="footer">
+                    <div className="footerLink" 
+                    onTouchStart={lighten} 
+                    onMouseOver={lighten} 
+                    onMouseLeave={dtLighten}>
+                    <Link onMouseLeave={dtLighten} 
+                    onTouchEnd={dtLighten} 
+                    to ="/portofolio">Portofolio</Link>
+                    </div>
+                    <div className="footerLink1" 
+                    onTouchStart={lighten} 
+                    onMouseOver={lighten} 
+                    onMouseLeave={dtLighten} >
+                    <Link onMouseLeave={dtLighten} 
+                     onTouchEnd={dtLighten}
+                      to ="/i">who I am</Link>
+                    </div>
+                    <div className="footerSocials">
+                    
+            
+                    </div>
+                </div>
+)
+
+}
---
+++ src/comp/iam.tsx
@@ -0,0 +1,21 @@
+import React from 'react' 
+
+export const I= () =>{
+
+
+    return (
+        <div className="about">
+           who I am? <br></br>
+            I am Comunications Engineer, my main profession is <br></br>
+       web development ,this is my first web site  <br></br>
+           but it's not the last, I am self taught <br></br>
+          most of my education from books, my retired will be in software section <br></br>
+        
+           what else:     <br></br>
+         I love to learn all new ,<br></br>
+          <br></br>
+            
+
+        </div>
+    )
+}
