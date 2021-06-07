e714f4e43c047e26a0d9cd7380c34f10c5f2e116
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2021-06-08 01:26:55 +0300

    Update about.tsx

--- src/comp/about.tsx
+++ src/comp/about.tsx
@@ -8,7 +8,7 @@ export const About= () =>{
           this site is a portofolio made with Node <br></br>
          for the backend and React for the front end <br></br>
         from <a href="https://github.com/ab3adel/Node-website">here</a> you can find the source code<br></br>
-       it will be updated and improvments periodicaly <br></br>
+       it will be updatings and improvments periodically <br></br>
 
         </div>
     )
