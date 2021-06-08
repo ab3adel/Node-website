4bc455fc1ae7301e5fc4a68f1843f039ef3c0684
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2021-06-08 22:56:37 +0300

    Update app.tsx

--- src/comp/app.tsx
+++ src/comp/app.tsx
@@ -8,6 +8,12 @@ import {I} from './iam'
 import github from '../images/github.png'
 import fb from '../images/facebook.png'
 import tele from '../images/telegram.png'
+import gate1 from '../images/python1.png'
+import gate2 from '../images/python2.png'
+import gate3 from '../images/js1.png'
+import gate4 from '../images/js2.png'
+import gate5 from '../images/cs1.png'
+import gate6 from '../images/cs2.png'
 
  class App extends React.Component <{},{}>{
        constructor (props:any) {
@@ -33,7 +39,9 @@ import tele from '../images/telegram.png'
                   
                     <Route exact path="/i"> <I/></Route>
                     <Route exact path="/about"> <About/></Route>
-                    <Route exact path="/portofolio"> <Home/></Route>
+                    <Route exact path="/portofolio"> <Home gate1={gate1} gate2={gate2} 
+                                                      gate3={gate3} gate4={gate4}
+                                                      gate5={gate5} gate6={gate6}/></Route>
                     <Route path="/">   
                        <Myself/>
                        <Footer/>
