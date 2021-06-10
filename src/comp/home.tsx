86a02120363223deca4453bbd303764305b66a2d
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2021-06-10 13:22:35 +0300

    Update home.tsx

--- src/comp/home.tsx
+++ src/comp/home.tsx
@@ -1,64 +1,37 @@
-import React ,{useEffect} from 'react'
+import React from 'react'
+import img from '../../images/c#.png'
 import * as d3 from 'd3'
-import {CS} from './projects/cs'
-import {JS} from './projects/js'
-import {Python} from './projects/python'
-
-export const Home =()=>{
-
-   const openGate= (e:Event)=>{
-      
-        let i:number=0;
-         if (scrollY >100 && scrollY <400){
-          i=0
-         }
-         else if ( scrollY >490 && scrollY<800){
-             i=1
-         }
-         else if (scrollY > 800){
-             i=2
-         }
-       
-          if (i === 2 ){
-           let lst = document.getElementsByClassName('section')
-           let target =lst.item(i) as HTMLDivElement
-           let text =target.getElementsByClassName('popUp')[0] as HTMLParagraphElement
-           text.style.animationName="inward"
-           setTimeout(()=>{
-               text.style.display="none"
-           },1800)
-          }
-       else {
-          let lst = document.getElementsByClassName('section')
-          let target =lst.item(i) as HTMLDivElement
-       let part1=target.getElementsByClassName('gate1')[0]
-       let part2 = target.getElementsByClassName('gate2')[0]
-       d3.select(part1).transition().style('transform','rotateX(90deg)').ease(d3.easeBounce).duration(2000)
-         .transition().style('height','1px').duration(2000).ease(d3.easeBounce).delay(400)
-         .style('z-index','0').delay(800)
-         .style('display','none')
-         d3.select(part2).transition().style('transform','rotateX(-90deg)').ease(d3.easeBounce).duration(2000)
-         .transition().style('height','1px').duration(2000).ease(d3.easeBounce).delay(400)
-         .style('display','none')
-        let gate =part1.parentElement as HTMLDivElement
- 
-        setTimeout(()=>{
-         gate.style.zIndex='0'
-         gate.style.display='none'
-        },3000)
-       }
-       
-      }
-      useEffect(()=>{
-        window.addEventListener('scroll',openGate)
-      },[])
-    return (
-    <div className="body">
-
-    <Python />
-    <JS  />
-    <CS />
+import {useState,useEffect} from 'react'
+import gate1 from '../../images/cs1.png'
+import gate2 from '../../images/cs2.png'
+export const CS =() =>{
+  
+    let [loaded,setLoaded]=useState(false)
    
+     const imgLoaded=()=>{
+      setLoaded(true)
+      document.querySelector('#detector')?.remove()
+     }
 
-   </div>)
+    return(
+        
+          <React.Fragment>
+              <img src={gate1} onLoad={imgLoaded} id="detector" style={{"display":"none"}}/>
+            {loaded?
+             <div className="sectioncs">
+              <div className="csGate">
+               <p className="popUp">soon !!</p>
+            <img src={gate1} className="csGate1"/>
+            <img src={gate2} className="csGate2"/>
+        
+            </div>
+            </div>: 
+            <div className="noScroll">
+           <span></span>
+           <span></span>
+           <span></span>
+         </div>
+            }
+         </React.Fragment>
+    )
 }
